09ec6d859797585604b8a5c0f16b4e3eb104eb95
Fanny <fannysarasoderlund@gmail.com>
2022-04-10 18:18:37 +0200

    added view education with courses taken

--- js/about.js
+++ js/about.js
@@ -1,5 +1,6 @@
 let funs;
 let funFactText;
+let index = 0;
 
 function funFact() {
 
@@ -12,12 +13,13 @@ function funFact() {
     'I enjoy folklore, old tales and other mythologies, especially Nordic and Greek',
     'Opinion: Cuphead\'s design is the definition of fine arts',
     'Most recently read book is \'Kirke\' by Madeline Miller',
-    'Recommended podcast: \'Mytologier\' by Munck for Spotify'
+    'Recommended podcast: \'Mytologier\' by Munck for Spotify',
+    'Opinion: Minimalism is for lazy people'
   ];
 
   funFactText = document.querySelector('#fun-fact-text')
 
-  funFactText.innerHTML = 'Press for a random fun fact about me'
+  funFactText.innerHTML = 'Press for a fun fact about me'
   const funFact = document.querySelector('.about-fun-fact');
 
   funFact.addEventListener('click', newFun);
@@ -26,17 +28,17 @@ function funFact() {
 
 function newFun() {
   const birds = document.getElementById('birds')
-  let i = (Math.random() * funs.length) | 0
-  if (funFactText.innerHTML === funs[i]) {
-    i = (i + 1) % funs.length;
-  }
-  funFactText.innerHTML = funs[i]
+  funFactText.innerHTML = funs[index]
 
-  console.log(funs[i])
-
-  if (i === 4) {
+  if (index === 4) {
     birds.style.opacity = 1
   } else {
     birds.style.opacity = 0
+  }
+
+  index++;
+
+  if (index === funs.length) {
+    index = 0;
   }
 }
