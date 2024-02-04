29a0675e4098f445ec583d1489c1d294ad6b3205
Fanny <fannysarasoderlund@gmail.com>
2024-02-04 10:30:02 +0100

    Updates Spring 24

--- js/about.js
+++ js/about.js
@@ -4,41 +4,41 @@ let index = 0;
 
 function funFact() {
 
-  funs = [
-    'My favourite artist is Ted Gärdestad',
-    'I made an egg hatching machine for a project in school and so far it has produced 21 chickens',
-    'I got a diving certificate and a flying certificate before I got my driver\'s licence',
-    'Before I started studying, I was in the restaurant business and co-owned a café and bar in Greece',
-    'I like to paint, but only birds',
-    'I enjoy folklore, old tales and other mythologies, especially Nordic and Greek',
-    'Opinion: Cuphead\'s design is the definition of fine arts',
-    'Most recently read book is \'Kirke\' by Madeline Miller',
-    'Recommended podcast: \'Mytologier\' by Munck for Spotify',
-    'Opinion: Minimalism is for lazy people'
-  ];
+    funs = [
+        'My favourite artist is Ted Gärdestad',
+        'I made an egg hatching machine for a project in school and it produced 21 chickens',
+        'I got a diving certificate and a flying certificate before I got my driver\'s licence',
+        'Before I got my degree, I was in the restaurant business and co-owned a café and bar in Greece',
+        'I like to paint, but only birds',
+        'I enjoy folklore, old tales and other mythologies, especially Nordic and Greek',
+        'Opinion: Cuphead\'s design is the definition of fine arts',
+        'Best book ever read is \'Kirke\' by Madeline Miller',
+        'Recommended podcast: \'Mytologier\' by Munck for Spotify',
+        'Opinion: Minimalism is for lazy people'
+    ];
 
-  funFactText = document.querySelector('#fun-fact-text')
+    funFactText = document.querySelector('#fun-fact-text')
 
-  funFactText.innerHTML = 'Press for a fun fact about me'
-  const funFact = document.querySelector('.about-fun-fact');
+    funFactText.innerHTML = 'Press for a fun fact about me'
+    const funFact = document.querySelector('.about-fun-fact');
 
-  funFact.addEventListener('click', newFun);
+    funFact.addEventListener('click', newFun);
 
 }
 
 function newFun() {
-  const birds = document.getElementById('birds')
-  funFactText.innerHTML = funs[index]
+    const birds = document.getElementById('birds')
+    funFactText.innerHTML = funs[index]
 
-  if (index === 4) {
-    birds.style.opacity = 1
-  } else {
-    birds.style.opacity = 0
-  }
+    if (index === 4) {
+        birds.style.opacity = 1
+    } else {
+        birds.style.opacity = 0
+    }
 
-  index++;
+    index++;
 
-  if (index === funs.length) {
-    index = 0;
-  }
+    if (index === funs.length) {
+        index = 0;
+    }
 }
